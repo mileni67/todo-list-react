eaaa015709eba80e75a1b6ddd8ca7b58c70eaad1
Oleksandr Vykhor <sashavihor67@gmail.com>
2023-01-03 22:19:12 +0100

    adding new task interaction

--- src/App.js
+++ src/App.js
@@ -37,10 +37,21 @@ function App() {
     })));
   }
 
+  const addNewTask = (content) => {
+    setTasks(tasks => [
+      ...tasks,
+       {
+        content,
+        done: false,
+        id: tasks.length === 0 ? 1 : tasks[tasks.length - 1].id + 1,
+       },
+      ]);
+  };
+
   return (
     <Container>
       <Header title="Lista zadań" />
-      <Section title="Dodaj nowe zadanie" body={<Form />} />
+      <Section title="Dodaj nowe zadanie" body={<Form addNewTask={addNewTask} />} />
       <Section
         title="Lista zadań"
         body={
--- src/Header/index.js
+++ src/Header/index.js
@@ -1,6 +1,5 @@
 import React from "react";
 
-
 const Header = ({title}) => (
     <header>
       <h1>{title}</h1>
